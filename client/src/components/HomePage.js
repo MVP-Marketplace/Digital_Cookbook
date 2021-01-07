cdd9d100e60fabe7100beafcd877934b279a9d33
wdillard <dillardwa@gmail.com>
2021-01-06 20:33:15 -0600

    Created Componenets and Added Routes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import SearchForm from './SearchForm'
 
 const HomePage = () => {
     return (
         <div>
-            
+            <h2>This is the Home Page</h2>
+            <SearchForm />
         </div>
     )
 }
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginPage = () => {
     return (
         <div>
-            
+            <h2>This is the Login Page</h2>
         </div>
     )
 }
