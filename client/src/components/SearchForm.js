e462a6e1ea5f0ee0ab9d38b6af2b9f41bba7a44f
JespsonSTP <jespstpierre@yahoo.com>
2021-02-24 10:35:24 -0500

    added the search form

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,23 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 import urlParams from './SpoonApiOptions';
+
 const SearchForm = () => {
+
+  // set the state for the search and the api data
+  const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const results = await axios.get(
-        `/api/search`,
-      );
-      setData(results.data.results);
-    };
-    getData();
-  }, []);
+  // function to make api call
+  const handleSearch = async () =>{
+    const results = await axios.get(
+      `/api/search?${search}`,
+    )
+    setData(results.data.results)
+  }
 
-  console.log(data)
+  // function handle submit from the form
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    handleSearch();
+  }
+
+  //function to handle the change in the search form 
+  const searchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
       <h2>This is the Search Form</h2>
+      <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          onChange={searchChange}
+        />
+        <br></br>
+        <input type="submit" id="submit" />
+      </form>
+      </div>
       <div>
         {data && data.length
           ? data.map((recipe) => <div key={recipe.id}>{recipe.title}</div>)
